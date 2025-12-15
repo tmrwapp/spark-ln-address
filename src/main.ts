53b67028cefe3ca45671676be024d60e1627f5ca
Bilthon <bilthon@gmail.com>
2025-12-14 22:23:53 -0500

    feat: extend initial logs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3003);
+  const port = process.env.PORT || 3003;
+  await app.listen(port);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
+  logger.log(`Environment: ${process.env.NODE_ENV}`);
+  logger.log(`Port: ${port}`);
+  logger.log(`Public Base URL: ${process.env.PUBLIC_BASE_URL}`);
 }
 bootstrap();
