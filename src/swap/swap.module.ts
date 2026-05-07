0c12d41e4871d8926e112391ea8ee2b8d33b0ee9
Bilthon <bilthon@gmail.com>
2026-05-06 23:56:34 -0500

    feat(swap): create refund case on DELIVERING->FAILED webhook

--- src/swap/swap.module.ts
+++ src/swap/swap.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 import { FlashnetModule } from '../flashnet/flashnet.module'
 import { PrismaService } from '../prisma/prisma.service'
+import { RefundCaseModule } from '../refund-case/refund-case.module'
 import { SwapService } from './swap.service'
 import { FlashnetWebhookController } from './flashnet-webhook.controller'
 
 @Module({
-  imports: [FlashnetModule],
+  imports: [FlashnetModule, RefundCaseModule],
   controllers: [FlashnetWebhookController],
   providers: [SwapService, PrismaService],
   exports: [SwapService],
